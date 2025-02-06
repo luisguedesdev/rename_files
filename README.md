270bed0035162e0e976b850fe3d9309c1033d781
luisguedesdev <luisgcostaguedes@gmail.com>
2025-02-06 17:52:49 -0300

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rename Files
+
+## Description
+
+This script renames videos files
+
+## Usage
+
+```bash
+python3 rename_files.py
+```
---
+++ rename-files.py
@@ -0,0 +1,24 @@
+import os
+
+def renomear_videos(diretorio_principal):
+    extensoes_video = ('.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.mpeg')
+    
+    for raiz, subdirs, arquivos in os.walk(diretorio_principal):
+        nome_pasta = os.path.basename(raiz)
+        videos = [arquivo for arquivo in arquivos if arquivo.lower().endswith(extensoes_video)]
+        
+        for idx, video in enumerate(videos, start=1):
+            extensao = os.path.splitext(video)[1]
+            novo_nome = f"{nome_pasta}{f'_{idx}' if len(videos) > 1 else ''}{extensao}"
+            caminho_antigo = os.path.join(raiz, video)
+            caminho_novo = os.path.join(raiz, novo_nome)
+            
+            try:
+                os.rename(caminho_antigo, caminho_novo)
+                print(f"Renomeado: {caminho_antigo} -> {caminho_novo}")
+            except Exception as e:
+                print(f"Erro ao renomear {caminho_antigo}: {e}")
+
+# Defina o caminho da pasta principal onde estão os vídeos
+diretorio = "E:\Downloads\AWS Impressionador"
+renomear_videos(diretorio)
